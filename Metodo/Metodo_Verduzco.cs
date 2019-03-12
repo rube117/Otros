fb6d82049d4fdbe65ee6d9c8807023ccdea015aa
rube117 <rube11_7@hotmail.com>
2019-03-12 15:20:06 -0600

    Add files via upload

---
+++ Metodo/Metodo_Verduzco.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Relojito
+{
+    public class Metodo_Verduzco
+    {
+        /*
+        System.DateTime moment = new System.DateTime(
+                                1999, 1, 13, 3, 57, 32, 11);
+        // Year gets 1999.
+        int year = moment.Year;
+
+        // Month gets 1 (January).
+        int month = moment.Month;
+
+        // Day gets 13.
+        int day = moment.Day;
+
+        // Hour gets 3.
+        int hour = moment.Hour;
+
+        // Minute gets 57.
+        int minute = moment.Minute;
+
+        // Second gets 32.
+        int second = moment.Second;
+
+        // Millisecond gets 11.
+        int millisecond = moment.Millisecond;
+        */
+    }
+}
